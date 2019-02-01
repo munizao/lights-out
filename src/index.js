44d3f2c8d95fd1dbbb4d44560db4cf813f70cf8f
Alexandre Muniz <aom@puzzlezapper.com>
2019-02-01 14:27:29 -0800

    Added clickability and light and dark squares.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+const board = document.getElementById('board');
+const boardSize = 4;
 
-function createSquares () {
+function clickSquare() {
+    if (this.classList.contains('lit')) {
+        this.classList.replace('lit', 'dark');
+    }
+    else {
+        this.classList.replace('dark', 'lit');
+    }
+}
 
+function createSquares() {
+    for(let i = 0; i < boardSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        board.appendChild(row); 
+        for(let j = 0; j < boardSize; j++) {
+            const square = document.createElement('span');
+            square.classList.add('square', 'lit');
+            row.appendChild(square);
+            square.addEventListener('click', clickSquare);
+        }
+    }
 }
 
 createSquares();
